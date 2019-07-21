1d574810a1534fb8be02152de81fa722884a5a3a
AMOREM <amorem@amorem.com>
2019-07-21 23:31:03 +0200

    custom button

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 import Header from "./components/header/Header";
 import HomePage from "./pages/homepage/HomePage";
 import ShopPage from "./pages/shop/ShopPage";
+import Sign from "./pages/sign/Sign";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/sign" component={Sign} />
       </Switch>
     </div>
   );
---
+++ src/pages/sign/Sign.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Sign.scss";
+import SignIn from "../../components/signin/SignIn";
+const Sign = () => (
+  <div className="sign-in-and-sign-up">
+    <SignIn />
+  </div>
+);
+
+export default Sign;
