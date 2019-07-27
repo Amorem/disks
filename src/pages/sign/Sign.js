3c6ab4804ae8fb12a6cc71eab600e1ae2e938e19
AMOREM <amorem@amorem.com>
2019-07-27 13:42:39 +0200

    SignUp Component

--- src/pages/sign/Sign.js
+++ src/pages/sign/Sign.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "./Sign.scss";
 import SignIn from "../../components/signin/SignIn";
+import SignUp from "../../components/signup/SignUp";
 const Sign = () => (
   <div className="sign-in-and-sign-up">
     <SignIn />
+    <SignUp />
   </div>
 );
 
